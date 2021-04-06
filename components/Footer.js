9207b073036fd367b8daee996f50f3c07007df15
Adebisi Ahmed <adebisi.ahmed10@gmail.com>
2021-04-06 22:49:01 +0100

    fixes

--- components/Footer.js
+++ components/Footer.js
@@ -1,6 +1,6 @@
 const Footer = () => (
   <div className="py-24 bg-dark flex justify-center">
-    <div className="inline-flex justify-between w-1/3 flex-wrap">
+    <div className="inline-flex justify-between w-full sm:w-1/3 flex-wrap">
       <a href="#" className="py-4 px-12 text-base">
         Fb
       </a>
