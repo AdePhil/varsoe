4aa1c788e576a58e1cde791ccaf032b8cc47e1fd
Adebisi Ahmed <adebisi.ahmed10@gmail.com>
2021-04-07 00:17:39 +0100

    fix white on scroll

--- _document.js
+++ _document.js
@@ -11,7 +11,6 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <link href="/fonts/style.css" rel="stylesheet" />
-          <script src="http://localhost:1337/vorlon.js" />
         </Head>
         <body>
           <Main />
