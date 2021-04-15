06667434905d8e4b5f551e052a87f827d0d30561
Adebisi Ahmed <adebisi.ahmed10@gmail.com>
2021-04-16 00:25:31 +0100

    footer links

--- components/Footer.js
+++ components/Footer.js
@@ -1,17 +1,14 @@
 const Footer = () => (
   <div className="py-24 bg-dark flex justify-center">
     <div className="inline-flex justify-between w-full sm:w-1/3 flex-wrap">
-      <a href="#" className="py-4 px-12 text-base">
-        Fb
-      </a>
-      <a href="#" className="py-4 px-12 text-base">
+      <a href="https://twitter.com/getvarsoe" className="py-4 px-12 text-base">
         Tw
       </a>
-      <a href="#" className="py-4 px-12 text-base">
+      <a href="https://www.linkedin.com/company/varsoe" className="py-4 px-12 text-base">
         Li
       </a>
-      <a href="#" className="py-4 px-12 text-base">
-        Lg
+      <a href="https://www.instagram.com/getvarsoe/" className="py-4 px-12 text-base">
+        Ig
       </a>
     </div>
   </div>
